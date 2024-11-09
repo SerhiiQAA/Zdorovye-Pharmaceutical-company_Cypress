742ebae695c969b2a4166f7bb16d00b6039b75fc
SerhiiQA <nabukve@gmail.com>
2024-11-09 14:30:02 +0200

    new

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
-    reportDir: 'cypress/reports',
+    reportDir: 'cypress/mochawesome-report',
     overwrite: true,
     html: true,
     json: false,
