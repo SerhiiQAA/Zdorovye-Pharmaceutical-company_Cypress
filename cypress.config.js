5ee8388582b0d281ad915e00ff00dfa8a1f6f5c0
SerhiiQA <nabukve@gmail.com>
2024-11-09 13:22:44 +0200

    reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,38 @@
 const { defineConfig } = require("cypress");
+const { merge } = require('mochawesome-merge');
+const generate = require('mochawesome-report-generator');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Merge mochawesome reports after all tests are done
+      on('after:run', async (results) => {
+        const options = {
+          files: [
+            './mochawesome-report/*.json',
+          ],
+        };
+
+        try {
+          const report = await merge(options);
+          console.log(report);
+          await generate.create(report, {
+            reportDir: 'mochawesome-report',
+            reportFilename: 'mochawesome-report',
+          });
+          console.log('HTML report generated');
+        } catch (error) {
+          console.error('Error merging and generating mochawesome reports:', error);
+        }
+      });
     },
-    baseUrl: ('https://zt.com.ua')
+    baseUrl: 'https://zt.com.ua',
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'mochawesome-report',
+      overwrite: true,  // перезаписує існуючі звіти
+      html: true,
+      json: false,  // не створює JSON звіти
+    }
   },
 });
